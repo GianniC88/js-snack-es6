e89770d4e02ebe26de175f72551e47833a1c2c0d
GianniC88 <gianni.colucci.88@gmail.com>
2025-06-11 17:43:29 +0200

    Aggiungi file HTML e JavaScript per snack-2 con logica per generare squadre di calcio

---
+++ snack-2/main.js
@@ -0,0 +1,25 @@
+/*
+Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”. Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+*/
+
+//array con squadre di calcio e proprietà :nome, punti fatti, falli subiti.
+const squadre = [
+  { nome: "squadra1", punti: 0, falliSubiti: 0 },
+  { nome: "squadra2", punti: 0, falliSubiti: 0 },
+  { nome: "squadra3", punti: 0, falliSubiti: 0 },
+  { nome: "squadra4", punti: 0, falliSubiti: 0 },
+  { nome: "squadra5", punti: 0, falliSubiti: 0 },
+  { nome: "squadra6", punti: 0, falliSubiti: 0 },
+  { nome: "squadra7", punti: 0, falliSubiti: 0 },
+];
+//ciclo di array per squadre
+for (let i = 0; i < squadre.length; i++) {
+  const squadra = squadre[i];
+  //generatore random di numeri (punti)
+  squadra.falliSubiti = Math.ceil(Math.random() * 100);
+  //generatore random di numeri(falli)
+  squadra.punti = Math.ceil(Math.random() * 25);
+}
+//stampa risultato
+console.log(squadre);
